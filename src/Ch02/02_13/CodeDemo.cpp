1e2cf2d3d12dce78e12367ce2aded9278b9b7bfb
oliviacchan <123203481+oliviacchan@users.noreply.github.com>
2025-02-10 16:22:40 +0000

    completed lesson 2

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -8,14 +8,14 @@
 int add_int(float a, double b, long double c){
     int result = 0;
 
-    // Write your code here
-    
+    result = (int)a + (int)b + (int)c;
+
     return result;
 }
 
 int main(){
-    float a = 2.1;
-    double b = 3.9;
+    float a = -2.0;
+    double b = -3.9;
     long double c = 4.6;
 
     int learnerResult = add_int(a, b, c);
